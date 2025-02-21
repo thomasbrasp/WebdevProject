f4193b74db459aa9a1a469582b1beb746c2aeb2c
thomasbrasp <thomasbrasp@outlook.com>
2025-02-21 18:28:51 +0100

    Refactor monsters.json with enriched data and expanded details
    Updated monster entries with unique IDs, expanded descriptions, and standardized attribute names like "location," "requirements," and "drops." Added missing monsters, fixed inaccuracies, and improved imagery URLs. This enhances clarity and ensures consistent game data representation.

--- milestone1/index.ts
+++ milestone1/index.ts
@@ -1,5 +1,5 @@
 import { Monsters} from "./types";
-import monsters from "../test.json"
+import monsters from "../archive/test.json"
 monsters.forEach(monster =>
     monster.id === 4 ? console.log(monster.name) : undefined)
     // console.log(monster.hasQuestItem))
--- milestone1/types.ts
+++ milestone1/types.ts
@@ -1,25 +1,27 @@
 export interface Monsters {
-    id:           number;
-    name:         string;
-    description:  string;
-    image:        string;
+    id: number;
+    name: string;
+    description: string;
+    image: string;
     requirements: Requirements;
-    location:     Location;
-    card:         Card;
-    drops:        string[];
-    birthdate:    Date;
+    location: Location;
+    cardId: number;
+    card?: Card;
+    drops: string[];
+    birthdate: Date;
     hasQuestItem: boolean;
 }
 
 export interface Card {
-    name:         string;
-    effect:       string;
+    id: number;
+    name: string;
+    effect: string;
     effectValues: number;
 }
 
 export interface Location {
     world: World;
-    area:  string;
+    area: string;
 }
 
 export enum World {
@@ -29,5 +31,5 @@ export enum World {
 
 export interface Requirements {
     accuracy: number | string;
-    defence:  number | string;
+    defence: number | string;
 }
