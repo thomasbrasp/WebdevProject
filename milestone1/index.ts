c10b469a3e2558658e294b8d264b5abe25168ef3
thomasbrasp <thomasbrasp@outlook.com>
2025-04-28 11:02:33 +0200

    Remove outdated monster JSON files
    Deleted `idleonMonsterConcept.json` and `idleonMonsters.json` as part of a cleanup process. These files were redundant and no longer relevant to the project, improving project structure and organization.

--- milestone1/index.ts
+++
@@ -1,6 +0,0 @@
-import { Monsters} from "./types";
-import monsters from "../archive/test.json"
-monsters.forEach(monster =>
-    monster.id === 4 ? console.log(monster.name) : undefined)
-    // console.log(monster.hasQuestItem))
-// console.log(monsters)
